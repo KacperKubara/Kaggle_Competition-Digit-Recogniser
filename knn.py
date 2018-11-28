c2375522a76c319bab2520ad37139566c5db189a
KacperKubara <kjk1u17@soton.ac.uk>
2018-11-28 19:28:42 +0000

    Initial changes

---
+++ knn.py
@@ -0,0 +1,8 @@
+import utils
+import pandas as pd 
+import numpy as np 
+import sklearn
+
+x_train, y_train = utils.read_train_data("train.csv")
+x_test = utils.read_test_data("test.csv")
+
---
+++ utils.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+
+def read_train_data(path):
+    x = pd.read_csv(path).drop(['label'], axis = 1)
+    y = pd.read_csv(path)['label']
+    return x, y:
+def read_test_data(path):
+    x = pd.read_csv(path)
+    return x
