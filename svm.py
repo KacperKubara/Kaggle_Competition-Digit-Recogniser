18adcfff7ca7dfe5af826d7c66addc67dae6007e
KacperKubara <kjk1u17@soton.ac.uk>
2018-11-30 11:55:14 +0000

    Explaining Thai how git works

--- svm.py
+++ svm.py
@@ -9,9 +9,9 @@ x_test = utils.read_test_data("test.csv")
 
 # SVC classiffier with polynomial kernel of 4th degree 
 from sklearn import svm
-svc = svm.SVC(kernel = 'poly', degree = 4)
+svc = svm.SVC(kernel = 'rbf')
 svc.fit(x_train, y_train)
 y_pred = svc.predict(x_test)
 
 # Save the data
-save_data("results.csv", y_pred)
+utils.save_data("results.csv", y_pred)
